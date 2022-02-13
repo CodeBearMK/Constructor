9bbdf35978e08d376e7d22d494db66e25ba8b20f
cat32tw <mike1993xd@gmail.com>
2022-02-13 16:48:08 +0800

    建構函式練習

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect1 = new Rectangle();
+            Rectangle rect2 = new Rectangle(107, 132);
+            Console.WriteLine();
+            Console.WriteLine("矩形長度:{0}",rect2.Length);
+            Console.WriteLine("矩形寬度:{0}", rect2.Wide);
+            Console.Write("矩形面積:{0}", rect2.GetArea(rect2.Length, rect2.Wide));
+            Console.Read();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    internal class Rectangle
+    {
+
+        public int Length { get; set; }
+        public int Wide { get; set; }
+
+        public Rectangle()
+        {
+            Console.WriteLine("已進入 Default Constructor...");
+        }
+        public Rectangle(int L, int W)
+        {
+            Length = L;
+            Wide = W;
+        }
+        public int GetArea(int L, int W)
+        {
+            return Length * Wide;
+        }
+    }
+}
